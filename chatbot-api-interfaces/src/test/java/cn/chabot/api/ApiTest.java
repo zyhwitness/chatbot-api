64f6e60b0d6d7712058739da12404102ce7f4ae1
zyhwitness <zyhwitness23@gmail.com>
2024-05-02 23:12:11 +0800

    zyhwitness, feat: api test ChatGPT

--- chatbot-api-interfaces/src/test/java/cn/chabot/api/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/chabot/api/ApiTest.java
@@ -97,6 +97,35 @@ public class ApiTest {
             System.out.println(response.getStatusLine().getStatusCode());
         }
     }
+
+    @Test
+    public void test_chatGPT() throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.openai.com/v1/chat/completions");
+
+        post.addHeader("Content-Type","application/json");
+        post.addHeader("Authorization","Bearer sess-9QaCVzlhqrUtEKkfaai3OpVyeSPyeMeseFGq5WPw");
+
+        String paramJson = "{\n" +
+                "     \"model\": \"gpt-3.5-turbo\",\n" +
+                "     \"messages\": [{\"role\": \"user\", \"content\": \"写一个冒泡排序\"}],\n" +
+                "     \"temperature\": 0.7\n" +
+                "   }";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String rsp = EntityUtils.toString(response.getEntity());
+            System.out.println(rsp);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 }
 
 
